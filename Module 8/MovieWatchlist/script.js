16e6a49dd197d06c89a3e55ea55948d107b5d9c1
KenjiroC <c.dueselder@live.de>
2024-01-25 23:23:03 +0100

    Update watchlist link and add watchlist page

--- Module 8/MovieWatchlist/script.js
+++ Module 8/MovieWatchlist/script.js
@@ -1,18 +1,28 @@
 const searchBtn = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
 const movieList = document.getElementById('movie-list');
-const searchBtn = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
-const movieList = document.getElementById('movie-list');
+
+document.addEventListener('click', (e) => {
+    if (e.target.dataset.add) {
+        e.preventDefault();
+        console.log(e.target.dataset.add)
+    } else if (e.target.dataset.remove) {
+        console.log(e.target.dataset.remove)
+    }
+});
+
+searchInput.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        handleSearchClick();
+    }
+});
+
+
 async function getMovieData() {
     console.log(searchInput.value, "searchInput.value from getMovieData()")
-    const response = await fetch(`http://www.omdbapi.com/?apikey=cd39d31c&s=${searchInput.value}`);
-    console.log(searchInput.value, "searchInput.value from getMovieData()")
-    const response = await fetch(`http://www.omdbapi.com/?apikey=cd39d31c&s=${searchInput.value}`);
+    const response = await fetch(`http://www.omdbapi.com/?apikey=cd39d31c&s=${searchInput.value}&type=movie`);
     const data = await response.json();
-    const movieData = await Promise.all(data.Search.map(movie => getMovieDetailData(movie.imdbID)));
-    console.log(movieData, "movieData from getMovieData()")
-    return movieData;
     const movieData = await Promise.all(data.Search.map(movie => getMovieDetailData(movie.imdbID)));
     console.log(movieData, "movieData from getMovieData()")
     return movieData;
@@ -38,14 +48,15 @@ async function renderMovieData() {
             </h5> 
             <p class="card-runtime">${movie.Runtime}</p>
             <p class="card-genre">${movie.Genre}</p>
-            <a href="#" class="card-add-watchlist">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <a href="#" class="card-add-watchlist" data-add="${movie.imdbID}">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" style="pointer-events: none;">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM9 5C9 4.44772 8.55228 4 8 4C7.44772 4 7 4.44772 7 5V7H5C4.44772 7 4 7.44771 4 8C4 8.55228 4.44772 9 5 9H7V11C7 11.5523 7.44772 12 8 12C8.55228 12 9 11.5523 9 11V9H11C11.5523 9 12 8.55228 12 8C12 7.44772 11.5523 7 11 7H9V5Z" fill="white"/>
                 </svg>
-                Add to Watchlist</a>
+                Add to Watchlist
+            </a>
             <p class="card-plot">${movie.Plot}</p>
         </div>
-        `
+        `;
     })
 }
 
